31e44fd7c81da771b2fe74bc4285e305da364529
Maria Fadeeva <4803328@gmail.com>
2023-06-14 19:52:55 +0300

    move git upper + add homework task1

---
+++ Lesson1.0/homework/task01/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.WriteLine("Enter the first number: ");
+string a = Console.ReadLine();
+int numberA = Convert.ToInt32(a);
+Console.WriteLine("Enter the second number: ");
+string b = Console.ReadLine();
+int numberB = Convert.ToInt32(b);
+if (numberA == numberB) 
+{
+    Console.WriteLine("numbers are seam");
+    return;
+}
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA+"(max) "+numberB+"(min)");
+}
+else
+{
+    Console.WriteLine(numberB+"(max) "+numberA+"(min)");
+}
