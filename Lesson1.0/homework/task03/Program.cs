15e85ee4c80c6732d1b581560cade7da77a21de1
Maria Fadeeva <4803328@gmail.com>
2023-06-14 21:19:46 +0300

    Added homework task3

---
+++ Lesson1.0/homework/task03/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Enter your number: ");
+string a = Console.ReadLine();
+int number = Convert.ToInt32(a);
+
+if (isEven(number))
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
+
+
+bool isEven(int a)
+{
+    return a % 2 == 0;
+}
