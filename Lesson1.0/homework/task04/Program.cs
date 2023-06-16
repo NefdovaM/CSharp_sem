a5354a63928da641984b8e6b227444a6526076f7
Maria Fadeeva <4803328@gmail.com>
2023-06-16 18:59:39 +0300

    Added homework task04 from 1 to N

--- Lesson1.0/homework/task04/Program.cs
+++ Lesson1.0/homework/task04/Program.cs
@@ -1,3 +1,17 @@
 ﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 5 -> 2, 4
 8 -> 2, 4, 6, 8*/
+
+Console.WriteLine("Enter your number from 1 to N: ");
+string a = Console.ReadLine();
+int n = Convert.ToInt32(a);
+int iterable = 1;
+
+while (iterable <= n)
+{
+    if (iterable % 2 == 0)
+    {
+        Console.WriteLine(iterable);
+    }
+    iterable++;
+}
