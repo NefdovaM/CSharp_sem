d7d301ca52acf462c1dfe786ad5f8712ab037691
Maria Fadeeva <4803328@gmail.com>
2023-06-19 19:40:34 +0300

    Upgrated homework task02

--- Lesson03/homework/task02/Program.cs
+++ Lesson03/homework/task02/Program.cs
@@ -2,3 +2,25 @@
 на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+Console.WriteLine("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+double powX = Math.Pow(x1 - x2, 2);
+double powY = Math.Pow(y1 - y2, 2);
+double powZ = Math.Pow(z1 - z2, 2);
+double result = Math.Sqrt(powX + powY + powZ);
+
+Console.WriteLine(result.ToString("0.##"));
