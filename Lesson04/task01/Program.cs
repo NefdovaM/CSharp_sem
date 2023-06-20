2d94f38d3fd9932aac5556b514a548b8a8ecc4e5
Maria Fadeeva <4803328@gmail.com>
2023-06-20 20:38:25 +0300

    принимает на вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ Lesson04/task01/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А
+
+7 -> 28
+4 -> 10
+8 -> 36 */
+
+int ReadInt()
+{
+    Console.Write("Введите целое число: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SumFrom1toA(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int number = ReadInt();
+int summa = SumFrom1toA(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {summa}");
