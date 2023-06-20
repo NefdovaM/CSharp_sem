2c84b065aa7f33caad5ee765523b387cec5c838b
Maria Fadeeva <4803328@gmail.com>
2023-06-20 21:33:29 +0300

    Принимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ Lesson04/task03/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.  
+4 -> 24   
+5 -> 120*/
+
+int ReadInt()
+{
+    Console.Write("Введите целое число: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int MultFrom1toA(int a)
+{
+    int mult = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+
+int number = ReadInt();
+int multiplication = MultFrom1toA(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} - {multiplication}");
