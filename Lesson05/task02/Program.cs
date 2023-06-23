a5dda29238a0ae4bb13f343c5be45ce7189045e0
Maria Fadeeva <4803328@gmail.com>
2023-06-23 21:15:39 +0300

    Added tasks 01 and 02

---
+++ Lesson05/task02/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу замена элементов массива: положительные элементы замените на соответствующие 
+отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]*/
+
+int[] getRandomArray()
+{
+    int[] array = new int[4];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(-9, 10);
+    }
+    Console.WriteLine("Вводный массив: ");
+    Console.WriteLine(string.Join(",", array));
+    return array;
+    
+}
+
+int [] array = getRandomArray();
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        array[i] = -array[i];
+    }
+    else if(array[i] < 0)
+    {
+        array[i] = -array[i];
+    }
+}
+
+Console.WriteLine("Измененный массив:");
+Console.WriteLine(string.Join(",", array));
