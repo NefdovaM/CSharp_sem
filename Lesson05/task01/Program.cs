26913616094949247a1b7aab4b221aee5d11f867
Maria Fadeeva <4803328@gmail.com>
2023-06-23 21:16:56 +0300

    положительные элементы замените на соответствующие отрицательные, и наоборот

---
+++ Lesson05/task01/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+сумма положительных чисел равна 29, сумма отрицательных равна -20.*/
+
+int[] getRandomArray()
+{
+    int[] array = new int[12];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(-9, 10);
+    }
+    return array;
+}
+
+int sumOfPositiveElements(int[] massive)
+{
+    int sumPositiveElements = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i] > 0)
+        {
+            sumPositiveElements += massive[i];
+        }
+    }
+    return sumPositiveElements;
+}
+int sumOfNegativeElements(int[] massive)
+{
+    int sumNegativeElements = 0;
+    for(int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i] < 0)
+        {
+            sumNegativeElements += massive[i];
+        }
+    }
+    return sumNegativeElements;
+
+}
+
+int [] myArray = getRandomArray();
+int sumPositive = sumOfPositiveElements(myArray);
+int sumNegative = sumOfNegativeElements(myArray);
+
+Console.WriteLine($"[{string.Join(", ", myArray)} ]");
+Console.WriteLine($"Сумма положительных элементов = {sumPositive}");
+Console.WriteLine($"Сумма отрицательных элементов = {sumNegative}");
