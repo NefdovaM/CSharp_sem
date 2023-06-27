39dca706eea4407da1de0ab6fce1fe4081654b68
Maria Fadeeva <4803328@gmail.com>
2023-06-27 21:55:13 +0300

    первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1

---
+++ Lesson06/task03/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8*/
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] Fibonacci(int n)
+{
+    int[] array = new int[n];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+Console.WriteLine(string.Join(", ", Fibonacci(n)));
