4e92dea58a5f1cf6524e27cd91c21cab45d4c91f
Maria Fadeeva <4803328@gmail.com>
2023-06-27 20:14:12 +0300

    массив заполненный случайными положительными трёхзначными числами. показывает количество чётных чисел в массиве

---
+++ Lesson05/homework/task01/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+Console.Write("Введите число элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] getRandomArray()
+{
+    int[] array = new int[a];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(99, 1000);
+    }
+    return array;
+}
+int[] myArray = getRandomArray();
+Console.WriteLine($"массив [{string.Join(", ", myArray)}]");
+
+int GetEvenCount(int[] array)
+{
+    int evenCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            evenCount = evenCount + 1;
+        }
+    }
+    return evenCount;
+}
+
+int evenCount = GetEvenCount(myArray);
+Console.WriteLine($"Количество чётных чисел в массиве: {evenCount}");
