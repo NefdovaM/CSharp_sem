80fb45db284abbe41f38de1d4c94d856976aa99a
NefdovaM <135155794+NefdovaM@users.noreply.github.com>
2023-06-28 13:15:31 +0300

    задаёт массив из 8 элементов и выводит их на экран

--- Lesson04/homework/task03/Program.cs
+++ Lesson04/homework/task03/Program.cs
@@ -1,1 +1,8 @@
 ﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.*/
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Random.Shared.Next(0,9);
+    Console.Write($"{array[i]} ");
+}
