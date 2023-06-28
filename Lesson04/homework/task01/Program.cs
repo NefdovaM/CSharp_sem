defcf6a58fef773700c6987d20d148dc62cc1e3a
NefdovaM <135155794+NefdovaM@users.noreply.github.com>
2023-06-28 13:12:55 +0300

    принимает на вход два числа (A и B) и возводит число A в натуральную степень B

--- Lesson04/homework/task01/Program.cs
+++ Lesson04/homework/task01/Program.cs
@@ -1,3 +1,17 @@
 ﻿/*Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. НЕ ИСПОЛЬЗОВАТЬ MATH.POW()
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = 1;
+while (b != 0)
+{
+    c = c * a;
+    b = b - 1;
+}
+
+Console.Write($"А в степени В равно {c}");
