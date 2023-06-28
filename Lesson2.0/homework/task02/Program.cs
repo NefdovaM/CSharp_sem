ef92942ac0a2291c2a984cca804d3b0ad109679c
NefdovaM <135155794+NefdovaM@users.noreply.github.com>
2023-06-28 13:04:28 +0300

    выводит третью цифру заданного числа

--- Lesson2.0/homework/task02/Program.cs
+++ Lesson2.0/homework/task02/Program.cs
@@ -2,3 +2,37 @@
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6 */
+
+start:
+Console.WriteLine("Введите любое число от 1 до 100000: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (99 < x && x < 999)
+{
+    int x1 = x % 10;
+    Console.WriteLine($"{x1} - третья цифра числа");
+}
+
+else if (999 < x && x < 10000)
+{
+    int x1 = x % 10;
+    int x2 = (x % 100 - x1) / 10;
+    Console.WriteLine($"{x2} - третья цифра числа");
+}
+
+else if (9999 < x && x < 100000)
+{
+    int x1 = x % 10;
+    int x2 = (x % 100 - x1) / 10;
+    int x3 = (x % 1000 - x2) / 100;
+    Console.WriteLine($"{x3} - третья цифра числа");
+}
+else if (x > 100000)
+{
+
+    Console.Write("Слишком длинное число. Попробуйте снова. ");
+    goto start;
+}
