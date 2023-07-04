a8c700a148cd4c9fc4561e9643cceb0dbcebca98
Maria Fadeeva <4803328@gmail.com>
2023-07-04 20:52:12 +0300

    заменяет строки на столбцы

---
+++ Lesson08/task02/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.*/
+
+const int rows = 4;
+const int columns = 4;
+int[,] GetRandomMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeRowsAndColumns(int[,] array)
+{
+    int rowCount = array.GetLength(0);
+    int columnCount = array.GetLength(1);
+    int[,] ar = new int[columnCount, rowCount];
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+        {
+            ar[i, j] = array[j, i];
+        }
+    }
+    return ar;
+}
+
+int[,] myMatrix = GetRandomMatrix(rows, columns);
+PrintMatrix(myMatrix);
+int[,] result = ChangeRowsAndColumns(myMatrix);
+Console.WriteLine();
+PrintMatrix(result);
