66458a6c90d05edf2346c08044f28dd2329ab83e
Maria Fadeeva <4803328@gmail.com>
2023-07-04 22:54:15 +0300

    удалит строку и столбец, на пересечении которых расположен наименьший элемент массива

--- Lesson08/task04/Program.cs
+++ Lesson08/task04/Program.cs
@@ -38,39 +38,35 @@ void PrintMatrixInts(int[,] matrix)
     }
 }
 
-int[] GetPositionMin(int[,] Matrix)
+int[,] RemoveRespectiveRowAndColumn(int[,] Matrix)
 {
+
     int iPositionMin = 0;
     int jPositionMin = 0;
-    int min = Matrix[0,0];
+    int min = Matrix[0, 0];
     for (int i = 0; i < Matrix.GetLength(0); ++i)
         for (int j = 0; j < Matrix.GetLength(1); ++j)
         {
-            if (Matrix[i,j] < min)
+            if (Matrix[i, j] < min)
             {
-                min = Matrix[i,j];
+                min = Matrix[i, j];
                 iPositionMin = i;
                 jPositionMin = j;
             }
         }
-    int[] arrayPair = new int[] {iPositionMin,jPositionMin};
-    return arrayPair;
-}
-
-int[,] RemoveRespectiveRowAndColumn(int[,] Matrix, int[] ArrayToRemove)
-{
-    int[,] matrixReduced = new int[Matrix.GetLength(0)-1, Matrix.GetLength(1)-1];
+    Console.WriteLine("i=" + iPositionMin + " j=" + jPositionMin);
+    int[,] matrixReduced = new int[Matrix.GetLength(0) - 1, Matrix.GetLength(1) - 1];
     int iShift = 0;
     int jShift = 0;
     for (int i = 0; i < matrixReduced.GetLength(0); ++i)
     {
-        if (i == ArrayToRemove[0])
-                iShift++;
+        if (i == iPositionMin)
+            iShift++;
         for (int j = 0; j < matrixReduced.GetLength(1); ++j)
         {
-            if (j == ArrayToRemove[1])
+            if (j == jPositionMin)
                 jShift++;
-            matrixReduced[i,j] = Matrix[i+iShift,j+jShift];
+            matrixReduced[i, j] = Matrix[i + iShift, j + jShift];
         }
         jShift = 0;
     }
@@ -78,8 +74,7 @@ int[,] RemoveRespectiveRowAndColumn(int[,] Matrix, int[] ArrayToRemove)
 }
 
 int[,] Matr = GetRandomMatrix(rows, columns);
-int[] array = GetPositionMin(Matr);
 PrintMatrixInts(Matr);
 Console.WriteLine();
-int[,] MatrReduced = RemoveRespectiveRowAndColumn(Matr, array);
+int[,] MatrReduced = RemoveRespectiveRowAndColumn(Matr);
 PrintMatrixInts(MatrReduced);
