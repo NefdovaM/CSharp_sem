0a2818a4b6ce381eca0081f70c0be6c1478a49a1
Maria Fadeeva <4803328@gmail.com>
2023-07-04 21:23:58 +0300

    частотный словарь элементов двумерного массива

--- Lesson08/task03/Program.cs
+++ Lesson08/task03/Program.cs
@@ -51,3 +51,38 @@ int a = (u++) + 5;
 Console.WriteLine(a);
 a = (++u) + 5;
 Console.WriteLine(a);
+
+/* 
+Console.WriteLine("Введите количество строк");
+int zz=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int rz=Convert.ToInt32(Console.ReadLine());
+int[,] a=new int[rz,zz];
+for(int i=0;i<rz;i++){
+    for(int j=0;j<zz;j++){
+        
+        a[i,j]=Random.Shared.Next(0,10);
+        
+
+        
+    }
+}
+for(int i=0;i<rz;i++){
+    for(int j=0;j<zz;j++){
+        Console.Write("{0,3}", a[i, j]);
+        
+    }
+    Console.WriteLine();
+}
+for(int g=0;g<10;g++){
+    int count=0;
+    for(int i=0;i<rz;i++){
+        for(int j=0;j<zz;j++){
+            if(a[i,j]==g){
+                count++;
+
+            }
+        }
+    }
+    Console.WriteLine($"{g} встречается {count} раз");}
+*/
