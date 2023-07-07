85c926ddf60ddcab1acc22dbb9b94086031eef74
Maria Fadeeva <4803328@gmail.com>
2023-07-07 21:35:30 +0300

    число и возвращать сумму его цифр

---
+++ Lesson09/task03/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр*/
+
+int number = 145;
+
+int digitsSum(int digit)
+{
+    if (digit <= 0) return 0;
+    int res = digitsSum(digit / 10) + (digit % 10);
+    return res;
+}
+
+Console.WriteLine(digitsSum(number));
