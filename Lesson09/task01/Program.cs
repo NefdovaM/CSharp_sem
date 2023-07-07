e0f37b62b63c6764b55eb933b06ddb52ecb6a501
Maria Fadeeva <4803328@gmail.com>
2023-07-07 20:49:51 +0300

    выведет все натуральные числа в промежутке от 1 до N

---
+++ Lesson09/task01/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"*/
+
+int number = 13;
+
+void naturalNumber(int lastNumber)
+{
+    if (lastNumber <= 0) return;
+    naturalNumber(lastNumber - 1);
+    Console.Write($"{lastNumber} ");
+}
+
+naturalNumber(number);
+Console.WriteLine();
