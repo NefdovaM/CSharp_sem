116a3638af5b634d96e6e415795d90d252516d1f
Maria Fadeeva <4803328@gmail.com>
2023-07-07 21:03:48 +0300

    выведет все натуральные числа в промежутке от M до N.

---
+++ Lesson09/task02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от M до N.
+
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"*/
+
+int numberM = 1;
+int numberN = 5;
+
+void naturalNumber(int m, int n)
+{
+    if (n < m) return;
+    naturalNumber(m, n - 1);
+    Console.Write($"{n} ");
+}
+
+naturalNumber(numberM, numberN);
+Console.WriteLine();
