cac4cf657c50066f17fc98dab1ae3ec607ee5ec5
Maria Fadeeva <4803328@gmail.com>
2023-07-07 21:44:20 +0300

    принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии

---
+++ Lesson09/task04/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8*/
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int pow (int number, int degree)
+{
+    if(degree == 0) return 1;
+    return pow(number, degree -1) * number;
+}
+
+Console.WriteLine(pow(A, B));
