8e1693a5293fec9fb99e7be16e9abdb478169619
Maria Fadeeva <4803328@gmail.com>
2023-07-10 18:14:23 +0300

    программа, которая будет находить строку с наименьшей суммой элементов

---
+++ Lesson08/homework/task02/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить 
+строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+const int rows = 4;
+const int columns = 4;
+int[,] GetRandomMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int MaxSumLine(int[,] matrix)
+{
+    int min = int.MaxValue;
+    int accum = 0;
+    int minIndex = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            accum += matrix[i, j];
+        }
+
+        if (accum < min)
+        {
+            min = accum;
+            minIndex = i;
+        }
+        accum = 0;
+    }
+    return minIndex;
+
+}
+
+int[,] myMatrix = GetRandomMatrix(rows, columns);
+PrintMatrix(myMatrix);
+
+int minIndex = MaxSumLine(myMatrix);
+
+Console.WriteLine($"Наименьшая сумма элементов в {minIndex} строке");
