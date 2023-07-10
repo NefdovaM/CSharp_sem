2c66a772128229dbf89e1a72aab226c4d9c4e2ed
Maria Fadeeva <4803328@gmail.com>
2023-07-10 17:40:59 +0300

    упорядочит по убыванию элементы каждой строки двумерного массива

---
+++ Lesson08/homework/task01/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+const int rows = 3;
+const int columns = 4;
+int[,] GetRandomMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+
+void GetChangeElements(int[,] array)
+{
+    int max = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            max = array[i,j];
+            for (int z = j + 1; z < array.GetLength(1); z++)
+            {
+                if(max < array[i, z])
+                {
+                    max = array[i, z];
+                    array[i,z] = array[i,j];
+                    array[i, j] = max;
+                }
+            }
+
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] myMatrix = GetRandomMatrix(rows, columns);
+PrintMatrix(myMatrix);
+GetChangeElements(myMatrix);
+Console.WriteLine();
+PrintMatrix(myMatrix);
