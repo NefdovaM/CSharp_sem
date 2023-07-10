61209223ba2092500a215f6d82f6d335e3de76ca
Maria Fadeeva <4803328@gmail.com>
2023-07-10 19:26:40 +0300

    трёхмерный массив из неповторяющихся двузначных чисел. построчно выводит массив, добавляя индексы каждого элемента

---
+++ Lesson08/homework/task04/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+
+const int rows = 2;
+const int columns = 2;
+const int tableNumber = 2;
+int[,,] GetRandomMatrix(int rows, int columns, int tableNumber)
+{
+    int[,,] matrix = new int[rows, columns, tableNumber];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = Random.Shared.Next (9, 100);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write($"{matrix[i, j, z]}({i},{j},{z})\t");
+            }
+                Console.WriteLine();
+        }
+
+    }
+}
+
+int[,,] myMatrix = GetRandomMatrix(rows, columns, tableNumber);
+PrintMatrix(myMatrix);
